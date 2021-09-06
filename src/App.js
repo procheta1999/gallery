5d92371ccbf4d49633485cb6df72df26d3a0a41b
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2021-09-07 01:23:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
+import 'antd/dist/antd.css';
+import {Row, Col, Image} from 'antd';
+import './App.css';
+const cache = {};
+
+function importAll(r) {
+    r.keys().forEach((key) => (cache[key] = r(key)));
+}
+// Note from the docs -> Warning: The arguments passed to require.context must be literals!
+importAll(require.context("./media", false, /\.(png|jpe?g|svg)$/));
+
+const images = Object.entries(cache).map(module => module[1].default);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <center style={{marginLeft:20}}>
+        <h1 className="typewriter-text" style={{letterSpacing:5}}>PICTURESQUEUE</h1>
+        <Row gutter={[, 48]}>
+        {images.map(image => (
+          <Col xs={{ span:18,offset:3 }} sm={{span:6,offset:1}} md={{span:6,offset:1}} lg={{ span:6,offset:1 }} xl={{span:6,offset:1}}>
+          <Image src={image}/>
+        </Col>
+         
+            ))}
+            </Row>
+        {/* <Row>
+      <Col xs={{ span:18,offset:3 }} sm={{span:6,offset:1}} md={{span:6,offset:1}} lg={{ span:6,offset:1 }} xl={{span:6,offset:1}}>
+        1 col-order-responsive
+      </Col>
+      <Col xs={{ span:18,offset:3 }} sm={{span:6,offset:1}} md={{span:6,offset:1}} lg={{ span:6,offset:1 }} xl={{span:6,offset:1}}>
+        2 col-order-responsive
+      </Col>
+      <Col xs={{ span:18,offset:3 }} sm={{span:6,offset:1}} md={{span:6,offset:1}} lg={{ span:6,offset:1 }} xl={{span:6,offset:1}}>
+        3 col-order-responsive
+      </Col>
+     
+    </Row> */}
+    </center>
+    
   );
 }
 
